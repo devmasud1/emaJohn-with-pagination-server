96f07c2074811807cc359f16d4ab53c1964b2e73
devmasud1 <devmahmud.masu1@gmail.com>
2023-11-02 16:58:18 +0600

    pagination api done

--- index.js
+++ index.js
@@ -30,8 +30,19 @@ async function run() {
     const productCollection = client.db('emaJhonDb').collection('products');
 
     app.get('/products', async(req, res) => {
-        const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page)
+      const size = parseInt(req.query.size)
+     
+        const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
         res.send(result);
+    })
+
+    app.get('/totalProducts', async(req, res) => {
+      const count = await productCollection.estimatedDocumentCount();
+      res.send({count})
     })
 
     // Send a ping to confirm a successful connection
